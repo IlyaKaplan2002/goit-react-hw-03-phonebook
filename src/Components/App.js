e5481ceef074c0ce2fe55dd6a270059ea5259538
Kaplan Ilya <kaplan11092002@gmail.com>
2022-01-19 16:45:36 +0200

    adding contacts to localStorage added

---
+++ src/Components/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import Filter from './Filter';
+import { v4 as uuid } from 'uuid';
+import Section from './Section';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    contacts && this.setState({ contacts: JSON.parse(contacts) });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  addContact = contact => {
+    const id = uuid();
+    const { name } = contact;
+
+    if (this.state.contacts.filter(contact => contact.name === name).length > 0) {
+      alert(`${name} is already in contacts`);
+      return;
+    }
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, { id, ...contact }],
+    }));
+  };
+
+  deleteItem = id => {
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts.filter(item => item.id !== id)],
+    }));
+  };
+
+  onFilterChange = e => {
+    const filter = e.target.value;
+    this.setState({ filter: filter.toLowerCase() });
+  };
+
+  getFilteredContacts = () => {
+    const { filter, contacts } = this.state;
+    return contacts.filter(({ name }) => name.toLowerCase().includes(filter));
+  };
+
+  render() {
+    return (
+      <>
+        <Section title="Phonebook">
+          <ContactForm onSubmit={this.addContact} />
+        </Section>
+        <Section title="Contacts">
+          <Filter onChange={this.onFilterChange} />
+
+          <ContactList onDelete={this.deleteItem} items={this.getFilteredContacts()} />
+        </Section>
+      </>
+    );
+  }
+}
+
+export default App;
